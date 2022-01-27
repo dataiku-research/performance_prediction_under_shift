47661dbd822ca4c0146b88f492ea5a10b97fbf3e
Simona Maggio <maggio.simona@gmail.com>
2022-01-27 21:10:37 +0100

    update pp viz

--- drift_dac_experiments/pp_viz.py
+++ drift_dac_experiments/pp_viz.py
@@ -7,7 +7,8 @@ import matplotlib.colors as mcolors
 
 
 def plot_metrics(out_fld, selected_ids, pp_colors, labels, metrics, all_runs_mae, figsize=(8, 5), suffix='',
-                 ylim_top=0.1):
+                 ylim_top=0.1, disaggregate=False):
+    plt.rcParams.update({'font.size': 16})
     all_runs_mae_mean = dict()
     for set_name in all_runs_mae:
         all_runs_mae_mean[set_name] = np.mean(all_runs_mae[set_name], axis=2)
@@ -25,7 +26,8 @@ def plot_metrics(out_fld, selected_ids, pp_colors, labels, metrics, all_runs_mae
             series_1 = series_1.append(pd.Series(all_runs_mae_mean[set_name][:, selected_ids[i]]), ignore_index=True)
             series_2 = series_2.append(pd.Series([set_name] * n_runs), ignore_index=True)
             series_3 = series_3.append(pd.Series(np.arange(n_runs)), ignore_index=True)
-            series_4 = series_4.append(pd.Series([pp] * n_runs), ignore_index=True)
+            series_4 = series_4.append(pd.Series([pp.replace('(amazon)', '[3]').replace('(naver)', '[2]')] * n_runs),
+                                       ignore_index=True)
 
     display_df = pd.DataFrame({'absolute error': series_1,
                                'set': series_2,
@@ -33,11 +35,11 @@ def plot_metrics(out_fld, selected_ids, pp_colors, labels, metrics, all_runs_mae
                                'Performance Predictor': series_4})
 
     plt.figure(figsize=figsize);
-    plt.title('Absolute Error of Performance Predictors')
+    # plt.title('Absolute Error of Performance Predictors')
     sns.boxplot(x="set", y="absolute error", hue="Performance Predictor",
                 data=display_df, palette=palette, fliersize=0)
-    plt.ylim(-0.001, ylim_top);
-    plt.legend(bbox_to_anchor=(1.4, 1.05));
+    plt.ylim(bottom=-0.001);  # , ylim_top);
+    plt.legend();  # bbox_to_anchor=(1.4, 1.05));
     plt.tight_layout()
     plt.savefig(os.path.join(out_fld, 'abs_error' + suffix + '.png'), bbox_inches='tight')
 
@@ -51,47 +53,52 @@ def plot_metrics(out_fld, selected_ids, pp_colors, labels, metrics, all_runs_mae
                 y = np.mean(metric_all, axis=1)
                 lo = np.quantile(metric_all, 0.1, axis=1)
                 hi = np.quantile(metric_all, 0.9, axis=1)
-                plt.plot(y, label=labels[i], color=pp_colors[i], marker='o', linewidth=2, markersize=8);
+                plt.plot(y, label=labels[i].replace('(amazon)', '[3]').replace('(naver)', '[2]'), color=pp_colors[i],
+                         marker='o', linewidth=2, markersize=8);
                 plt.fill_between(np.arange(len(all_runs_metric.keys())), lo, hi, alpha=0.3, color=pp_colors[i])
-        plt.legend(bbox_to_anchor=(1.4, 1.05));
+        plt.legend();  # bbox_to_anchor=(1.2, 1.05));
         plt.xticks(range(len(all_runs_metric.keys())), all_runs_metric.keys());
         # plt.ylim(-0.1, 1.1);
-        plt.hlines(y=0.0, xmin=0, xmax=len(all_runs_metric.keys()), linestyles='dashed');
-        plt.title("%s of Performance Predictors" % name);
-        plt.ylabel('%s' % name);
+        # plt.hlines(y=0.0, xmin=0, xmax=len(all_runs_metric.keys()), linestyles='dashed');
+        # plt.title("%s of Performance Predictors" % name);
+        plt.ylabel('%s' % name.replace('auc_sigma_mae', r'$MAE_{CI}$').replace('within_ci_mae', r'$MAE_{CI_{0.05}}$'));
 
         plt.tight_layout()
         plt.savefig(os.path.join(out_fld, '%s%s.png' % (name, suffix)), bbox_inches='tight')
 
     # desaggregated results
+    if disaggregate:
 
-    for run in range(n_runs):
+        for run in range(n_runs):
 
-        series_1 = pd.Series()
-        series_2 = pd.Series()
-        series_3 = pd.Series()
-        series_4 = pd.Series()
+            series_1 = pd.Series()
+            series_2 = pd.Series()
+            series_3 = pd.Series()
+            series_4 = pd.Series()
 
-        for i, pp in enumerate(labels[selected_ids]):
-            for set_name in all_runs_mae:
-                series_1 = series_1.append(pd.Series(all_runs_mae[set_name][run, selected_ids[i], :]),
-                                           ignore_index=True)
-                series_2 = series_2.append(pd.Series([set_name] * all_runs_mae[set_name].shape[2]), ignore_index=True)
-                series_3 = series_3.append(pd.Series(np.arange(all_runs_mae[set_name].shape[2])), ignore_index=True)
-                series_4 = series_4.append(pd.Series([pp] * all_runs_mae[set_name].shape[2]), ignore_index=True)
+            for i, pp in enumerate(labels[selected_ids]):
+                for set_name in all_runs_mae:
+                    series_1 = series_1.append(pd.Series(all_runs_mae[set_name][run, selected_ids[i], :]),
+                                               ignore_index=True)
+                    series_2 = series_2.append(pd.Series([set_name] * all_runs_mae[set_name].shape[2]),
+                                               ignore_index=True)
+                    series_3 = series_3.append(pd.Series(np.arange(all_runs_mae[set_name].shape[2])), ignore_index=True)
+                    series_4 = series_4.append(pd.Series(
+                        [pp.replace('(amazon)', '[3]').replace('(naver)', '[2]')] * all_runs_mae[set_name].shape[2]),
+                                               ignore_index=True)
 
-        display_df = pd.DataFrame({'absolute error': series_1,
-                                   'set': series_2,
-                                   'run': series_3,
-                                   'Performance Predictor': series_4})
+            display_df = pd.DataFrame({'absolute error': series_1,
+                                       'set': series_2,
+                                       'run': series_3,
+                                       'Performance Predictor': series_4})
 
-        plt.figure(figsize=figsize);
-        plt.title('Absolute Error of Performance Predictors %d' % run)
-        sns.boxplot(x="set", y="absolute error", hue="Performance Predictor",
-                    data=display_df, palette=palette)
-        plt.legend(bbox_to_anchor=(1.4, 1.05));
-        plt.tight_layout()
-        plt.savefig(os.path.join(out_fld, 'abs_error' + suffix + '_%d.png' % run), bbox_inches='tight')
+            plt.figure(figsize=figsize);
+            # plt.title('Absolute Error of Performance Predictors %d' % run)
+            sns.boxplot(x="set", y="absolute error", hue="Performance Predictor",
+                        data=display_df, palette=palette)
+            plt.legend();  # bbox_to_anchor=(1.2, 1.05));
+            plt.tight_layout()
+            plt.savefig(os.path.join(out_fld, 'abs_error' + suffix + '_%d.png' % run), bbox_inches='tight')
 
 
 def pp_viz(fld, out_fld, subpop=True, ylim_top=0.1, figsize=(8, 5), seed=42):
@@ -106,9 +113,10 @@ def pp_viz(fld, out_fld, subpop=True, ylim_top=0.1, figsize=(8, 5), seed=42):
     all_runs_auc_sigma_acc = dict()
     all_runs_auc_sigma_mae = dict()
 
-    np.random.seed(seed)
+    np.random.seed(12345)
 
-    pp_colors = np.random.choice(list(mcolors.CSS4_COLORS), size=n_pp, replace=False)
+    # pp_colors = np.random.choice(list(mcolors.CSS4_COLORS), size=n_pp, replace=False)
+    pp_colors = list(mcolors.CSS4_COLORS)[10::7]
 
     if subpop:
         # set_names = ['train', 'test_no_shift', 'test', 'test_unseen', 'test_unseen_subpop', 'test_natural']
@@ -187,3 +195,11 @@ def pp_viz(fld, out_fld, subpop=True, ylim_top=0.1, figsize=(8, 5), seed=42):
 
     plot_metrics(out_fld, paper_models_ids, pp_colors, labels, metrics, all_runs_mae, figsize=figsize,
                  suffix='_paper', ylim_top=ylim_top)
+
+    # Paper 2
+
+    paper_models_ids = [11, 2, 3, 5]
+
+    plot_metrics(out_fld, paper_models_ids, pp_colors, labels, metrics, all_runs_mae, figsize=figsize,
+                 suffix='_paper2', ylim_top=ylim_top)
+
